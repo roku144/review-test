21233521d1ef2430f4db98b48ba322fa67e8b935
roku144 <dubhashi2@gmail.com>
2021-01-06 15:58:09 -0500

    Update input-service.php

--- input-service.php
+++ input-service.php
@@ -16,26 +16,19 @@ if ($conn -> connect_errno) {
 }
 
 $names = $_POST["names"];
-$grades = $_POST["grades"];
+//$grades = $_POST["grades"];
 
-$color = $_POST["color"];
+//$color = $_POST["color"];
 $cook= $_POST["cook"];
 $act = $_POST["act"];
-$star = $_POST["star"];
+//d$star = $_POST["star"];
 
 //creating the individual date and time sent$dt=date("F j, Y, g:i a");
 
 $ip=$_SERVER['REMOTE_ADDR'];
 
-echo($names);
-echo($grades);
-echo($color);
-echo($cook);
-echo($act);
-echo($star);
-
-$stmt = $conn->prepare ("INSERT INTO testing_dba (names, age, color, cookie, activity, rate, ip) VALUES (?, ?, ?, ?, ?, ?, ?)");
-$stmt->bind_param("sisssis", $names, $grades, $color, $cook, $act, $star, $ip);
+$stmt = $conn->prepare ("INSERT INTO testing_dba (names, cookie, activity, ip) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $names, $cook, $act, $ip);
 $stmt->execute();
 
 ?>
